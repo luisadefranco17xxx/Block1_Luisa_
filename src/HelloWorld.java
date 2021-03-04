7fb63d5df0664ff97fba03c1653f7202f5eb5ce0
Luisa <massimo.e.luisa.au@gmail.com>
2021-03-04 11:10:42 +0100

    ll

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+//Kommentar
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word ttt -34321");
+        // etwas ausgeben print ls(neue zeile)
+
+    }
+}
